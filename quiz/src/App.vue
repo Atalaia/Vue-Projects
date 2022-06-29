d445fb3126f5a6d2f8ec63f8015e6e02245f7ab2
Maria Soage <maria.soage01@gmail.com>
2022-06-29 16:34:30 +0200

    Improve Questions component

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length" :questions="questions" />
+    <questions v-if="questionsAnswered < questions.length" :questions="questions" :questionsAnswered="questionsAnswered"
+      @question-answered="questionAnswered" />
     <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
@@ -19,6 +20,7 @@ export default {
   data() {
     return {
       questionsAnswered: 0,
+      totalCorrect: 0,
       questions: [
         {
           q: 'What is 2 + 2?',
@@ -94,6 +96,14 @@ export default {
           desc: "Studying has definitely paid off for you!"
         }
       ]
+    };
+  },
+  methods: {
+    questionAnswered(is_correct) {
+      if (is_correct) {
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
     }
   }
 }
