9180009f6eb4113d4355be2fcbb073d069bf7624
Maria Soage <maria.soage01@gmail.com>
2022-07-01 23:32:25 +0200

    music app first commit

---
+++ music/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'max-len': 'off',
+    'vuejs-accessibility/alt-text': 'off',
+    'no-trailing-spaces': 'off',
+    'vuejs-accessibility/label-has-for': 'off',
+    'eol-last': 'off',
+    'vue/no-multiple-template-root':'off',
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+};
