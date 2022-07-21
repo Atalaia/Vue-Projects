baf8abb322c4a814b42139abfbba4e372a40f4ad
Maria Soage <maria.soage01@gmail.com>
2022-07-21 18:21:18 +0200

    Refactoring, using actions,  persisting the user authentication and setting up the login

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -81,6 +81,20 @@
       </vee-field>
       <ErrorMessage class="text-red-600" name="country" />
     </div>
+    <!-- Category -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Category</label>
+      <vee-field
+        as="select"
+        name="category"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      >
+        <option value="Artist">Artist</option>
+        <option value="Listener">Listener</option>
+        <option value="Both">Both</option>
+      </vee-field>
+      <ErrorMessage class="text-red-600" name="category" />
+    </div>
     <!-- TOS -->
     <div class="mb-3 pl-6">
       <vee-field
@@ -103,8 +117,6 @@
 </template>
 
 <script>
-import firebase from "@/includes/firebase";
-
 export default {
   name: "RegisterForm",
   data() {
@@ -116,10 +128,12 @@ export default {
         password: "required|min:6|max:100|alpha_spaces",
         confirm_password: "passwords_mismatch:@password",
         country: "required|country_excluded:Antarctica",
+        category: "required",
         tos: "tos",
       },
       userData: {
         country: "Spain",
+        category: "Listener",
       },
       reg_in_submission: false,
       reg_show_alert: false,
@@ -134,11 +148,8 @@ export default {
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created. ";
 
-      let userCred = null;
       try {
-        userCred = await firebase
-          .auth()
-          .createUserWithEmailAndPassword(values.email, values.password);
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
@@ -148,7 +159,6 @@ export default {
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
-      console.log(userCred);
     },
   },
 };
--- music/src/includes/firebase.js
+++ music/src/includes/firebase.js
@@ -1,10 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  
+
 };
 
 // Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection("users");
+
+export { auth, db, usersCollection };
