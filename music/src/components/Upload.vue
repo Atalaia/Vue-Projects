b1413225340b53dc575ea6326539d95fc9e9b871
Maria Soage <maria.soage01@gmail.com>
2022-08-08 22:57:39 +0200

    Reading and editing list of songs from firebase, adding validation to edit form

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -116,6 +116,11 @@ export default {
 
       console.log(files);
     },
+    cancelUploads() {
+      this.uploads.forEach((upload) => {
+        upload.task.cancel();
+      });
+    },
   },
   beforeUnmount() {
     this.uploads.forEach((upload) => {
